c512d72953db700d1c1d702c370bec808b9e0750
Paul Rivière <paulriviere002@gmail.com>
2022-10-30 00:46:43 +0200

    add login and logout system

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/sign_up', name: 'app_register')]
+    #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
